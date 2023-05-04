3eb721c96d640cb1701ce29e16ec9965f01fe7e2
FictionFics <salvi.pm91@gmail.com>
2023-05-04 19:46:41 +0000

    Initial commitgi

---
+++ projects/pgbackup/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgpackup',
+    version='0.1.0',
+    author='Fiction Fics',
+    author_email='salvi.pm91@gmail.com',
+    description='A utility for backing up PostgreSQL databases',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/FictionFics/Python.git',
+    packages=find_packages('src'),
+)
